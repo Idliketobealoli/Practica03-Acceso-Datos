f79551277dd6dacf78a20be124380c5f1f63c4f4
Daniel Rodríguez Muñoz <daniel.rodriguez@alumno.iesluisvives.org>
2021-12-01 14:22:58 +0100

    asfghergdnhtftgfeghftdgnhgtryesgfn htrfgmytrgdfg

--- src/main/kotlin/dto/DepartmentDTO.kt
+++ src/main/kotlin/dto/DepartmentDTO.kt
@@ -8,20 +8,43 @@ import javax.xml.bind.annotation.*
 
 @XmlRootElement(name = "department")
 @XmlAccessorType(XmlAccessType.FIELD)
-class DepartmentDTO (
-        @XmlAttribute
-        var id: String,
-        var name: String,
-        var boss: Programmer,
-        var budget: Double,
-        @XmlElementWrapper
-        var finishedProjects: List<Project>?,
-        @XmlElementWrapper
-        var developingProjects: List<Project>?,
-        var anualBudget: Double,
-        @XmlElementWrapper
-        var bossHistory: List<Programmer>
-) {
+class DepartmentDTO () {
+    @XmlAttribute
+    lateinit var id: String
+    lateinit var name: String
+    lateinit var boss: Programmer
+    var budget: Double = 0.0
+
+    @XmlElementWrapper
+    var finishedProjects: List<Project>? = null
+
+    @XmlElementWrapper
+    var developingProjects: List<Project>? = null
+    var anualBudget: Double = 0.0
+
+    @XmlElementWrapper
+    lateinit var bossHistory: List<Programmer>
+
+    constructor(
+            id: String,
+            name: String,
+            boss: Programmer,
+            budget: Double,
+            finishedProjects: List<Project>?,
+            developingProjects: List<Project>?,
+            anualBudget: Double,
+            bossHistory: List<Programmer>
+    ) : this() {
+        this.id = id
+        this.name = name
+        this.boss = boss
+        this.budget = budget
+        this.finishedProjects = finishedProjects
+        this.developingProjects = developingProjects
+        this.anualBudget = anualBudget
+        this.bossHistory = bossHistory
+    }
+
     fun fromJSON(json : String) : DepartmentDTO? {
         return Gson().fromJson(json, DepartmentDTO::class.java)
     }
